56e5dc995fb273fdea0249088db029438c86c192
linhhonblade <meomaimaimeo@gmail.com>
2021-02-18 18:18:41 +0700

    Force sender email

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import mail_mail_monkeypatch
+from . import ir_mail_server_monkeypatch
+# from . import mail_message_monkeypatch
---
+++ models/mail_message_monkeypatch.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# import logging
+# import re
+#
+# from binascii import Error as binascii_error
+# from collections import defaultdict
+# from operator import itemgetter
+#
+# from odoo import _, api, fields, models, modules, tools
+# from odoo.exceptions import AccessError
+# from odoo.http import request
+# from odoo.osv import expression
+# from odoo.tools import groupby
+#
+# _logger = logging.getLogger(__name__)
