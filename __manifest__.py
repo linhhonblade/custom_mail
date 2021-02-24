342f5464cdcadf8146bf7a13919dfdf52abcbf79
linhhonblade <meomaimaimeo@gmail.com>
2021-02-24 17:19:16 +0700

    [IMP] Unfold the activity detail in chatter

--- __manifest__.py
+++ __manifest__.py
@@ -26,12 +26,13 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        # 'views/views.xml',
-        # 'views/templates.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'qweb': [
+        'views/qweb.xml',
+    ]
 }
